bf4404f739b0680e22c153e4fe86c69c3d7b4d67
Randy <randy.zhang@youtile.com>
2016-08-04 11:23:52 +0800

    commit user model and controller

---
+++ SuperRandy/application/controllers/api/User.php
@@ -0,0 +1,195 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Created by PhpStorm.
+ * User: RandyZhang
+ * Date: 16/8/1
+ * Time: 下午2:02
+ */
+class User extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('user_model');
+    }
+
+    /**
+     * 登录
+     */
+    public function login()
+    {
+        // 获取客户端传递过来的参数
+        $username = $this->input->post('username');
+        $psw = $this->input->post('psw');
+        $platform = $this->input->post('platform');
+        $data['username'] = $username;
+        $data['psw'] = $psw;
+        $data['platform'] = $platform;
+        // 判断用户名是否存在
+        if ($this->user_model->is_user_exist($username)) {// 存在
+            $login_result = $this->user_model->login($data);
+            if ($login_result['code'] == 1) {
+                HCommon::output_json(array(
+                    'code' => 1,
+                    'message' => "登录成功",
+                    'user_info' => $login_result['user_info'],
+                ));
+            } else {
+                HCommon::output_json([
+                    'code' => 0,
+                    'message' => $login_result['message'],
+                    'data' => $data,
+                ]);
+            }
+        } else {// 不存在
+            HCommon::output_json(array(
+                'code' => 0,
+                'message' => "登录失败,该账号不存在",
+                'data' => $data,
+            ));
+        }
+        // 手机端登录
+        if (strtolower($platform) == 'android' || strtolower($platform) == 'ios') {
+//            $result_array = $this->user_model->
+        }
+    }
+
+    /**
+     * 注册
+     */
+    public function register()
+    {
+        $username = $this->input->post('username');
+        $psw = $this->input->post('psw');
+//        $platform = $this->input->post('platform');
+        $data['username'] = $username;
+        $data['psw'] = $psw;
+
+        if ($this->user_model->is_user_exist($username)) {
+            HCommon::output_json(array(
+                'code' => 0,
+                'message' => "注册失败,该账号已存在",
+                'data' => $data
+            ));
+        } else {
+            if ($this->user_model->register($data)) {
+                HCommon::output_json(array(
+                    'code' => 1,
+                    'message' => "注册成功",
+                    'data' => $data
+                ));
+            } else {
+                HCommon::output_json(array(
+                    'code' => 0,
+                    'message' => "注册失败",
+                    'data' => $data
+                ));
+            }
+        }
+    }
+
+    /**
+     * 删除用户信息
+     */
+    public function deleteUserInfo()
+    {
+        $id = $this->input->post('id');
+        $delete_result = $this->user_model->delete_user_info($id);
+        if ($delete_result == -1) {
+            HCommon::output_json(array(
+                'code' => 0,
+                'message' => "删除失败",
+                'data' => $id
+            ));
+        } else if ($delete_result == 0) {
+            HCommon::output_json(array(
+                'code' => 0,
+                'message' => "删除失败,对应的id不存在",
+                'data' => $id
+            ));
+        } else {
+            HCommon::output_json(array(
+                'code' => 1,
+                'message' => "删除成功",
+                'data' => $id
+            ));
+        }
+    }
+
+    /**
+     * 获取用户信息
+     */
+    public function getUserInfo()
+    {
+        $id = $this->input->post('id');
+        $result_array = $this->user_model->get_user_info($id);
+        if ($result_array['code'] == 0) {
+            HCommon::output_json(array(
+                'code' => 0,
+                'message' => $result_array['message'],
+                'data' => $id
+            ));
+        } else {
+            HCommon::output_json(array(
+                'code' => 1,
+                'message' => $result_array['message'],
+                'data' => $result_array['user_info']
+            ));
+        }
+    }
+
+    /**
+     * 更新用户信息, 可能更新user的若干个字段
+     */
+    public function updateUserInfo()
+    {
+        $data['id'] = $this->input->post('id');
+        $data['username'] = $this->input->post('username');
+        $data['psw'] = $this->input->post('psw');
+        $data['avatar'] = $this->input->post('avatar');
+
+        $result = $this->user_model->update_user_info($data);
+        if ($result < 1) {
+            HCommon::output_json(array(
+                'code' => 0,
+                'message' => "用户信息更新失败"
+            ));
+        } else {
+            HCommon::output_json(array(
+                'code' => 1,
+                'message' => "用户信息更新成功"
+            ));
+        }
+    }
+
+    /**
+     * 获取用户列表
+     */
+    public function getUserList()
+    {
+        $result = $this->user_model->get_user_info_list();
+        if ($result->num_rows() < 1) {
+            HCommon::output_json(array(
+                'code' => 0,
+                'message' => "获取用户列表失败"
+            ));
+        } else {
+            $user_array = $result->result_array();
+            HCommon::output_json(array(
+                'code' => 1,
+                'message' => "获取用户列表成功",
+                'data' => $user_array
+            ));
+        }
+    }
+
+    /**
+     * 登出
+     */
+    public function logout()
+    {
+
+    }
+}
---
+++ SuperRandy/application/models/User_model.php
@@ -0,0 +1,133 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Created by PhpStorm.
+ * User: RandyZhang
+ * Date: 16/8/1
+ * Time: 下午1:50
+ */
+class User_model extends CI_Model
+{
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * 增加用户(用于注册)
+     * @param $data
+     * @return bool
+     */
+    public function add_user_info($data)
+    {
+        $this->db->insert('sr_user', $data);
+        $insert_id = $this->db->insert_id();
+        if ($insert_id) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * @param $id
+     */
+    public function delete_user_info($id)
+    {
+        // todo 注销用户,一般不推荐使用该方法
+        $this->db->where('id', $id);
+        $this->db->delete('sr_user');
+        return $this->db->affected_rows();
+    }
+
+    /**
+     * @param array
+     */
+    public function update_user_info($data)
+    {
+        $this->db->where('id', $data['id']);
+        $this->db->update('sr_user', $data);
+        return $this->db->affected_rows();
+    }
+
+    /**
+     * 获取用户信息
+     * @param $id $id 用户id
+     * @return array
+     */
+    public function get_user_info($id)
+    {
+        $sql = "select * from `sr_user` where id=?";
+        $condition_value = array($id);
+        $result_row = $this->db->query($sql, $condition_value);
+        if ($result_row->num_rows() < 1) {
+            return array('code' => 0, 'message' => '获取用户信息失败');
+        } else {
+            return array('code' => 1, 'message' => '获取用户信息成功', 'user_info' => $result_row->row());
+        }
+    }
+
+    /**
+     * 获取所有用户信息
+     */
+    public function get_user_info_list()
+    {
+        $result = $this->db->get('sr_user');
+        return $result;
+    }
+
+    /**
+     * 判断用户名是否存在
+     * @param $username
+     * @return bool
+     */
+    public function is_user_exist($username)
+    {
+        $sql = "select * from `sr_user` where username=?";
+        $condition_value = array($username);
+        $result_row = $this->db->query($sql, $condition_value);
+        if ($result_row->num_rows() < 1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * 登录
+     * @param  array
+     * @return mixed
+     */
+    public function login($data)
+    {
+        /**
+         * todo 判断login_type
+         *  移动客户端->移动端登录处理
+         *  PC端->PC端登录处理
+         */
+        $username = $data['username'];
+        $psw = $data['psw'];
+        $platform = $data['platform'];
+
+        $sql = "select * from `sr_user` where username=?";
+        $condition_value = array($username);
+        $result_row = $this->db->query($sql, $condition_value);
+        $user_row = $result_row->row();
+        if ($user_row->psw != $psw) {
+            return array('code' => 0, 'message' => '登录密码不正确');
+        } else {
+            $user_info['id'] = $user_row->id;
+            $user_info['username'] = $user_row->username;
+            $user_info['psw'] = $user_row->psw;
+            $user_info['avatar'] = $user_row->avatar;
+            return array('code' => 1, 'message' => '登录成功', 'user_info' => $user_info);
+        }
+    }
+
+    public function register($data)
+    {
+        return $this->add_user_info($data);
+    }
+}
