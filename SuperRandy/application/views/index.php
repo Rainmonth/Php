c0dad458d8933522ddf321bbdedfff2b6afa3a06
Randy <rainmonth@yeah.net>
2016-08-25 16:33:51 +0800

    index页面修改

--- SuperRandy/application/views/index.php
+++ SuperRandy/application/views/index.php
@@ -1,84 +1,81 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
     <title>荏苒追寻</title>
-
-    <style type="text/css">
-
-        ::selection {
-            background-color: #00FF00;
-            color: white;
-        }
-
-        ::-moz-selection {
-            background-color: #808000;
-            color: white;
-        }
-
-        body {
-            background-color: #fff;
-            margin: 40px;
-            font: 13px/20px normal Helvetica, Arial, sans-serif;
-            color: #4F5155;
-        }
-
-        h1 {
-            color: #444;
-            background-color: transparent;
-            border-bottom: 1px solid #D0D0D0;
-            font-size: 19px;
-            font-weight: normal;
-            margin: 0 0 14px 0;
-            padding: 14px 15px 10px 15px;
-        }
-
-        p.content {
-            text-align: center;
-            font-size: 16px;
-            padding: 0 10px 0 10px;
-
-        }
-
-        p.footer {
-            text-align: right;
-            font-size: 11px;
-            border-top: 1px solid #D0D0D0;
-            line-height: 32px;
-            padding: 0 10px 0 10px;
-            margin: 20px 0 0 0;
-        }
-
-        #container {
-            margin: 10px;
-            border: 1px solid #D0D0D0;
-            box-shadow: 0 0 8px #D0D0D0;
-        }
+    <link type="text/css" rel="stylesheet" href="assets/css/base.css">
+    <style>
+        
     </style>
 </head>
 <body>
 
-<div id="container">
-    <h1>欢迎来到rainmonth.com</h1>
+<section class="feature-overview">
+    <div class="container">
+        <ul class="features">
+            <li class="feature">
+                <a href="#" rel="external">
+                    <img class="screen-shot" src="assets/images/bg.jpg" alt="屏幕截图">
+                </a>
+                <h2>登录背景页</h2>
+                <p>
+                    登录背景页面详细说明
+                </p>
+                <a class="more" href="Welcome/navToApiDemo" rel="external">
+                    Api Demo页面
+                </a>
+            </li>
+            <li class="feature">
+                <a href="#" rel="external">
+                    <img class="screen-shot" src="assets/images/bg.jpg" alt="屏幕截图">
+                </a>
+                <h2>登录背景页</h2>
+                <p>
+                    登录背景页面详细说明
+                </p>
+                <a class="more" href="Welcome/navToApiIndex" rel="external">Api Index页面</a>
+            </li>
+            <li class="feature">
+                <a href="#" rel="external">
+                    <img class="screen-shot" src="assets/images/bg.jpg" alt="屏幕截图">
+                </a>
+                <h2>登录背景页</h2>
+                <p>
+                    登录背景页面详细说明
+                </p>
+                <a class="more" href="Welcome/navToApiDemo" rel="external">Api Demo页面</a>
+            </li>
+            <li class="feature">
+                <a href="#" rel="external">
+                    <img class="screen-shot" src="assets/images/bg.jpg" alt="屏幕截图">
+                </a>
+                <h2>登录背景页</h2>
+                <p>
+                    登录背景页面详细说明
+                </p>
+                <a class="more" href="Welcome/navToApiDemo" rel="external">Api Demo页面</a></li>
+            <li class="feature">
+                <a href="#" rel="external">
+                    <img class="screen-shot" src="assets/images/bg.jpg" alt="屏幕截图">
+                </a>
+                <h2>登录背景页</h2>
+                <p>
+                    登录背景页面详细说明
+                </p>
+                <a class="more" href="Welcome/navToApiDemo" rel="external">Api Demo页面</a></li>
+            <li class="feature">
+                <a href="#" rel="external">
+                    <img class="screen-shot" src="assets/images/bg.jpg" alt="屏幕截图">
+                </a>
+                <h2>登录背景页</h2>
+                <p>
+                    登录背景页面详细说明
+                </p>
+                <a class="more" href="Welcome/navToApiDemo" rel="external">Api Demo页面</a></li>
+        </ul>
+    </div>
 
-    <p class="content">
-        网站主题内容
-    </p>
-    <p class="content">
-        <a href="Welcome/navToApiDemo">Api Demo页面</a>
-    </p>
-    <p class="content">
-        <a href="Welcome/navToApiIndex">Api Index页面</a>
-    </p>
-    <p class="content">
-        <a href="Welcome/navToApiIndex">Api Index页面</a>
-    </p>
-    <p class="footer">Page rendered in <strong>{elapsed_time}</strong>
-        seconds. <?php echo (ENVIRONMENT === 'development') ? 'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?>
-    </p>
-</div>
+</section>
 
 </body>
 </html>
